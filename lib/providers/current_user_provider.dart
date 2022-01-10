6d0f73c23938f97bad985cea59433d1befb918d9
stefanzindovic <stefanzindovicc@gmail.com>
2022-01-10 14:22:12 +0100

    feat: Include context of current user provider

--- lib/providers/current_user_provider.dart
+++ lib/providers/current_user_provider.dart
@@ -1,3 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
-class CurrentUserProvider extends ChangeNotifier {}
+class CurrentUserProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? currentUser;
+
+  Stream<User?> get user => _auth.authStateChanges().map((User? firebaseUser) {
+        currentUser = firebaseUser;
+        notifyListeners();
+      });
+}
