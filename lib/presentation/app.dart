1e22fc7ffea5bb148100f0ad0467f5a63a29a706
stefanzindovic <stefanzindovicc@gmail.com>
2022-01-15 19:24:26 +0100

    feat: Implement function for delete user account

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:memories/presentation/widgets/authentication_wrapper.dart';
--- lib/repository/user_informations.dart
+++ lib/repository/user_informations.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:memories/models/user.dart';
 
@@ -9,6 +10,7 @@ class UserInformations {
       FirebaseFirestore.instance.collection('users');
   static final Reference _storage =
       FirebaseStorage.instance.ref().child('profile_pictures');
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
 
   static Future<void> insertUserInfo(UserModel user) async {
     await _collection.doc(user.uid).set(user.toJson());
@@ -26,5 +28,14 @@ class UserInformations {
   static Future<String> uploadProfilePicture(String uid, File photo) async {
     final TaskSnapshot upload = await _storage.child(uid).putFile(photo);
     return upload.ref.getDownloadURL();
+  }
+
+  static Future<void> deleteUserAccount() async {
+    final userData = await getUserInfo(_auth.currentUser!.uid);
+    if (userData?.profilePhotoUrl != null) {
+      await _storage.child(_auth.currentUser!.uid).delete();
+    }
+    await _collection.doc(_auth.currentUser!.uid).delete();
+    await _auth.currentUser!.delete();
   }
 }
