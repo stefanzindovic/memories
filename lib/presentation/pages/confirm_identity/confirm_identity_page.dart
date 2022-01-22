fbd48298ccad9f9974573d9452198c6226332980
stefanzindovic <stefanzindovicc@gmail.com>
2022-01-22 15:35:08 +0100

    feat: Create and implement function for refresh user credentials before delete user account

--- lib/presentation/pages/confirm_identity/confirm_identity_page.dart
+++ lib/presentation/pages/confirm_identity/confirm_identity_page.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:memories/providers/current_user_provider.dart';
 import 'package:memories/theme/colors.dart';
+import 'package:provider/provider.dart';
 
 enum RefreshUserAuthenticationStatus {
   initial,
@@ -26,6 +29,7 @@ class ConfirmIdentityPage extends StatefulWidget {
 
 class _ConfirmIdentityPageState extends State<ConfirmIdentityPage> {
   String _password = '';
+  String _email = '';
   String? errorMessage;
   RefreshUserAuthenticationStatus _refreshUserAuthenticationStatus =
       RefreshUserAuthenticationStatus.initial;
@@ -33,8 +37,40 @@ class _ConfirmIdentityPageState extends State<ConfirmIdentityPage> {
       DeleteUserAccountStatus.initial;
   final _formKey = GlobalKey<FormState>();
 
+  Future<RefreshUserAuthenticationStatus> refreshUserAuthentication() async {
+    RefreshUserAuthenticationStatus status =
+        RefreshUserAuthenticationStatus.initial;
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: _email, password: _password);
+      status = RefreshUserAuthenticationStatus.success;
+      print('Sve je u redu!');
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      if (e.code == 'user-not-found') {
+        errorMessage =
+            'Korisnik sa tom e-mail adresom ne postoji. Molimo vas da pokušate sa drugom e-mail adresom ili da kreirate novi korisniški račun.';
+      } else if (e.code == 'wrong-password') {
+        errorMessage =
+            'Navedeni podaci za prijavljivanje se ne poklapaju. Molimo vas da pokušate ponovo.';
+      } else if (e.code == 'network-request-failed') {
+        errorMessage =
+            'Internet konekcija nije ostvarena. Molimo vas da provjerite vašu internet vezu i pokušajte ponovo.';
+      }
+      status = RefreshUserAuthenticationStatus.error;
+    } catch (e) {
+      print(e);
+      status = RefreshUserAuthenticationStatus.error;
+      errorMessage =
+          'Trenutno nismo u mogućnosti da deaktiviramo vaš korisnički nalog. Molimo vas da pokušate ponovo malo kasnije.';
+    }
+    return status;
+  }
+
   @override
   Widget build(BuildContext context) {
+    setState(
+        () => _email = Provider.of<CurrentUserProvider>(context).credentials!);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Deaktoivacija naloga'),
@@ -92,19 +128,52 @@ class _ConfirmIdentityPageState extends State<ConfirmIdentityPage> {
                               minimumSize: Size(110.w, 65.h),
                             ),
                             onPressed: () async {
-                              if (_formKey.currentState!.validate()) {}
+                              if (_formKey.currentState!.validate()) {
+                                setState(() =>
+                                    _refreshUserAuthenticationStatus =
+                                        RefreshUserAuthenticationStatus
+                                            .loading);
+                                final RefreshUserAuthenticationStatus result =
+                                    await refreshUserAuthentication();
+                                if (result ==
+                                    RefreshUserAuthenticationStatus.error) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      backgroundColor: backgroundColor,
+                                      content: Text(
+                                        errorMessage.toString(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2,
+                                      ),
+                                    ),
+                                  );
+                                }
+                                setState(() =>
+                                    _refreshUserAuthenticationStatus =
+                                        RefreshUserAuthenticationStatus
+                                            .initial);
+                              }
                             },
-                            child: Row(
-                              children: [
-                                const Text('Dalje'),
-                                SizedBox(
-                                  width: 5.w,
-                                ),
-                                const Icon(
-                                  FeatherIcons.arrowRight,
-                                ),
-                              ],
-                            ),
+                            child: (_refreshUserAuthenticationStatus ==
+                                    RefreshUserAuthenticationStatus.loading)
+                                ? SizedBox(
+                                    width: 24.w,
+                                    height: 24.h,
+                                    child: const CircularProgressIndicator(
+                                        color: lightColor),
+                                  )
+                                : Row(
+                                    children: [
+                                      const Text('Dalje'),
+                                      SizedBox(
+                                        width: 5.w,
+                                      ),
+                                      const Icon(
+                                        FeatherIcons.arrowRight,
+                                      ),
+                                    ],
+                                  ),
                           ),
                         ],
                       ),
