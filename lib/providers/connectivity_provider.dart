7bb424af356742e0c7defe04a2a19a14cbbdf110
stefanzindovic <stefanzindovicc@gmail.com>
2022-01-23 14:38:24 +0100

    feat: Create function to init connectivity tracker

--- lib/providers/connectivity_provider.dart
+++ lib/providers/connectivity_provider.dart
@@ -1,3 +1,25 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-class ConnectivityProvider extends ChangeNotifier {}
+class ConnectivityProvider extends ChangeNotifier {
+  Connectivity _connectivity = Connectivity();
+  bool _isOnline = false;
+  bool get isOnline => _isOnline;
+
+  Future<void> initConnectivity() async {
+    try {
+      var status = await _connectivity.checkConnectivity();
+
+      if (status == ConnectivityResult.none) {
+        _isOnline = false;
+        notifyListeners();
+      } else {
+        _isOnline = true;
+        notifyListeners();
+      }
+    } on PlatformException catch (e) {
+      print("Connectivity provider error: ${e.toString()}");
+    }
+  }
+}
